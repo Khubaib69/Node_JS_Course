8e279d74a707f4900c7bca7b2cf480012ecd588c
Khubaib Idrees <khubaib.idrees@itb.local>
2023-03-07 18:42:16 +0500

    added route path and also some description

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const app = express();
 
 // middleware 1 will consoellog but will not go the next muddkeware bcz it is stuck there
 // next() will transfer the sequence of control to the next middleware
-app.use((req,res,next)=>{
-    console.log("I am middle ware 1 ...");
+
+app.use('/', (req, res, next) => {
+    console.log("This always run");
     next();
 })
 
-app.use((req,res,next)=>{
+app.use('/add-product', (req, res, next) => {
+    console.log("I am middle ware 1 ...");
+    res.send('<h1>HELLO FROM Product Page</h1>')
+})
+
+// any route other than /add-product will lead to the following path
+app.use('/', (req, res, next) => {
     console.log("I am middle ware 2 ...");
     res.send('<h1>HELLO FROM EXPRESS</h1>')
 })

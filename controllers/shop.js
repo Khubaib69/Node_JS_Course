6d6ec0b625ca72e17e5c0f2919891b51ee0ece1a
Khubaib Idrees <khubaib.idrees@itb.local>
2023-03-15 16:43:29 +0500

    final commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const { route } = require('../routes/admin');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll().then(([rows, fieldData]) => {
@@ -15,74 +16,78 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
-};
-
-exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
+  Product.findById(prodId)
+    .then(([rows]) => {
+      res.render('shop/product-detail', {
+        product: rows[0],
+        pageTitle: rows.title,
+        path: '/products'
       });
     })
-    .catch(err => { console.log(err) })
-};
+    .catch((e) => {
+      console.log(e);
+    })
+  }
 
-exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
+  exports.getIndex = (req, res, next) => {
+    Product.fetchAll()
+      .then(([rows, fieldData]) => {
+        res.render('shop/index', {
+          prods: rows,
+          pageTitle: 'Shop',
+          path: '/'
+        });
+      })
+      .catch(err => { console.log(err) })
+  };
+
+  exports.getCart = (req, res, next) => {
+    Cart.getCart(cart => {
+      Product.fetchAll(products => {
+        const cartProducts = [];
+        for (product of products) {
+          const cartProductData = cart.products.find(
+            prod => prod.id === product.id
+          );
+          if (cartProductData) {
+            cartProducts.push({ productData: product, qty: cartProductData.qty });
+          }
         }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          products: cartProducts
+        });
       });
     });
-  });
-};
+  };
 
-exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
-};
-
-exports.postCartDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
+  exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+      Cart.addProduct(prodId, product.price);
+    });
     res.redirect('/cart');
-  });
-};
+  };
 
-exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
+  exports.postCartDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+      Cart.deleteProduct(prodId, product.price);
+      res.redirect('/cart');
+    });
+  };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+  exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders'
+    });
+  };
+
+  exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+      path: '/checkout',
+      pageTitle: 'Checkout'
+    });
+  };
--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'password@123'
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'password@123',
+    port: 3306,
 });
 
 module.exports = pool.promise();
